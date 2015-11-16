550705014014ffa971d9646148cda5bc7269d076
Shayne Chow <shayne@weplus.cn>
2015-11-16 17:54:43 +0800

    Initial Commit

---
+++ TomatoTimer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TomatoTimer
+//
+//  Created by Xiang on 15/11/16.
+//  Copyright © 2015年 微加科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
