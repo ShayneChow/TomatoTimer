b2774dfe2ff4f8ae0b23952ed6de0acde84040fa
Shayne Chow <shayne@weplus.cn>
2015-11-21 20:25:19 +0800

    计时按钮的重绘

--- TomatoTimer/Classes/ZXTimerViewController.m
+++ TomatoTimer/Classes/ZXTimerViewController.m
@@ -8,8 +8,11 @@
 
 #import "ZXTimerViewController.h"
 #import "MZTimerLabel.h"
+#import "UIView+XMGExtension.h"
 
 @interface ZXTimerViewController ()
+
+@property (strong, nonatomic) IBOutlet UIButton *startButton;
 
 @end
 
@@ -18,21 +21,46 @@
 @synthesize easyTimer = _easyTimer;
 @synthesize smallTimer = _smallTimer;
 
+//static  testTime
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self setupStartButton];
     
     easyTimer = [[MZTimerLabel alloc] initWithLabel:_easyTimer andTimerType:MZTimerLabelTypeTimer];
-    [easyTimer setCountDownTime:1500];
+    [easyTimer setCountDownTime:10];
     easyTimer.resetTimerAfterFinish = NO;
     easyTimer.delegate = self;
     easyTimer.timeFormat = @"mm:ss";
     
     smallTimer = [[MZTimerLabel alloc] initWithLabel:_smallTimer andTimerType:MZTimerLabelTypeTimer];
-    [smallTimer setCountDownTime:1500];
+    [smallTimer setCountDownTime:10];
     smallTimer.resetTimerAfterFinish = NO;
     smallTimer.delegate = self;
     smallTimer.timeFormat = @"SS";
+}
+
+- (void)setupStartButton {
+
+    // 图片
+    _startButton.imageView.centerX = _startButton.width * 0.5;
+    // _startButton.imageView.x = 0;
+    _startButton.imageView.y = 0;
+    
+    // 文字
+    _startButton.titleLabel.x = 0;
+    _startButton.titleLabel.y = _startButton.imageView.height;
+    _startButton.titleLabel.width = _startButton.width;
+    _startButton.titleLabel.height = _startButton.height - _startButton.imageView.height;
+}
+
+/**
+ *  状态栏的样式
+ */
+- (UIStatusBarStyle)preferredStatusBarStyle {
+    // 白色状态栏
+    return UIStatusBarStyleLightContent;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -42,6 +70,7 @@
 
 - (IBAction)timerStart {
     if(![easyTimer counting] && ![smallTimer counting]){
+        _startButton.hidden = YES;
         _easyTimer.textColor = [UIColor colorWithWhite:0.298 alpha:1.000];
         _smallTimer.textColor = [UIColor colorWithWhite:0.298 alpha:1.000];
         [easyTimer reset];
@@ -56,11 +85,13 @@
     if([timerLabel isEqual:easyTimer]) {
         UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"再接再厉" message:msg delegate:nil cancelButtonTitle:@"恭喜完成!" otherButtonTitles:nil];
         [alertView show];
+        
+        _startButton.hidden = NO;
     }
 }
 
 - (void)timerLabel:(MZTimerLabel *)timerLabel countingTo:(NSTimeInterval)time timertype:(MZTimerLabelType)timerType{
-    if(time < 301) {
+    if(time < 5) {
         timerLabel.timeLabel.textColor = [UIColor colorWithRed:1.000 green:0.400 blue:0.400 alpha:1.000];
     }
 }
